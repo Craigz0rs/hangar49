7afd72e64d75c11c87460d7378f5ec59c9e24d80
Craig D'Arcy <cdarcy@live.ca>
2017-11-15 22:11:29 -0800

    Created base php files

---
+++ front-page.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package Hangar_49
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
