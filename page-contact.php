985448828057bd80e6fafbd055dc1d5c936f37e1
Craig D'Arcy <cdarcy@live.ca>
2017-12-04 12:10:25 -0800

    Made changes to contact page, began SASS styling

--- page-contact.php
+++ page-contact.php
@@ -17,22 +17,23 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-			<?php
-			while ( have_posts() ) : the_post();
-
-				get_template_part( 'template-parts/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-			endwhile; // End of the loop.
-			?>
+            <div class="contact_splash">
+                <h1>Contact Us</h1>
+                <p>Interested in one of our planes or looking for more information on the services we offer?</p>
+                <p>Give us a shout!</p>
+            </div>
+            <div class="contact_info">
+                <tel></tel>
+                <email>contact@hangar49warbirds.com</email>
+            </div>
+            <div class="contact_form">
+                <?php echo do_shortcode('[contact-form-7 id="87" title="Contact form 1"]'); ?>
+            </div>
+            
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+
 get_footer();
