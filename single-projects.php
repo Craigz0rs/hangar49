a9692c287670690a37548bfe222c1461ca07098f
Craig D'Arcy <cdarcy@live.ca>
2018-02-12 12:55:17 -0800

    Styling individual projects

--- single-projects.php
+++ single-projects.php
@@ -69,7 +69,39 @@ get_header('archive'); ?>
                         } 
                 ?>
             <div class="page_header_wrapper" id="aircraft_header_wrapper">
-                <div class="page_header" id="aircraft_page_header">
+                <div class="page_header" id="aircraft_page_header" style="background-image: url(<?php echo $featured_image; ?>)">
+                    <div class="page_header_overlay" id="news_page_header_overlay">
+                    </div>
+                    <h1><?php echo $year . ' ' . $manufacturer . ' ' . $model; ?></h1>
+                    <div id="project_header_info_box" class="header_info_box">
+                        <span>
+                            <p class="aircraft_header_serial"><?php 
+                                if($serial_number){
+                                    echo 'S/N: ' . $serial_number . ' • ';
+                                }
+                                if($registration_number){
+                                    echo 'R/N: ' . $registration_number;
+                                }
+                                if($price){
+//                                    echo '<br>' . $price;
+                                } ?>   
+                            </p>
+                            <p class="special_info"><?php
+                                if($special_info){
+                                    echo $special_info;
+                                } ?>
+                            </p>
+                        </span>
+                    </div>
+                        <div class="archive_sorting project_sorting" id="aircraft_sorting">
+                            <a href="#project_section" class="smoothScroll">see project</a>
+                            <a href="#gallery" class="smoothScroll">view gallery</a>
+                            <a href="#aircraft_info" class="smoothScroll">see details</a>
+                        </div>
+                    
+<!--                    <p class="now_viewing">project</p>-->
+
+<!--                    
                     <div class="flexslider gallery_slider" id="featured_slider">
                     <?php if( have_rows('featured_image_gallery') ) { ?>
                     <div class="page_header_overlay" id="featured_slider_overlay">
@@ -138,23 +170,16 @@ get_header('archive'); ?>
                         </div>
                         </div>
                     </div>
+ -->                   
                 </div>
             </div>
-            <div class="project_section" id="project_section">
-<!--
-                 <div class="aircraft_section_heading">
-                    <div class="aircraft_content_wrap">
-                        <h2>project</h2>
-                    </div>
-                </div>
--->
-<!--                <span class="aircraft_section_line"></span>-->
-                <div class="page_main_wrapper" id="news_page_main_wrapper">
+            <div class="project_section" id="project_section">        
+                <div class="page_main_wrapper" id="aircraft_page_main_wrapper">
                     <div class="article_listings_wrapper" id="project_wrapper">
                         <div class="detail_item" id="overview">
                             <strong class="detail_title">Overview:</strong>
                             <p class="detail_value"><?php echo $project_overview; ?></p>
-                        </div>
+                        </div>      
                         <?php
                             if( $work_update_entries ) {
                                 foreach( $work_update_entries as $row ){
@@ -162,16 +187,33 @@ get_header('archive'); ?>
                                     $entry_date = $row['entry_date'];
                                     $work_entry = $row['work_entry']; 
                                     $work_gallery = $row['project_update_gallery']; ?>
-                                    <article class="project_post">
-                                        <div class="article_side">
-                                            
-                                            <p><?php echo $entry_date; ?></p>
+                                      
+                        <article class="project_post">
+                            <div class="article_side">
+                                <p><?php echo $entry_date; ?></p>
+                            </div>
+                            
+                            <div class="article_preview_content">
+                                <header class="article_header" id="single_news_post_header"> 
+                                     <div class="article_title">
+                                        <h2><?php echo $entry_title; ?></h2>
+                                    </div>
+                                    <div class="entry-meta">
+                                        <ul class="article_meta_list">
+                                            <li><i class="far fa-user"></i> by <?php echo get_the_author('display_name'); ?></li>
+                                            <li><i class="far fa-calendar"></i> on  <?php echo $entry_date; ?></li>
+                                        </ul>
+<!--
+                                        <div class="article_category_list">
+                                            <i class="fas fa-tags"></i> <?php echo get_the_category_list(); ?>
                                         </div>
-                                        <div class="detail_with_slider project_post_content">
-                                            <div class="detail_value project_post_text">
-                                                <h3><?php echo $entry_title; ?></h3>
+-->
+                                    </div><!-- .entry-meta -->
+                                </header> 
+                                <div class="news_article_content">
+                                                
                                                 <p><?php echo $work_entry; ?></p>
-                                            </div>
+                                </div>
                                             <?php if( $work_gallery ) { ?>
                                             <div class="flexslider gallery_slider" id="project_slider">
                                                 <ul class="slides gallery_slides">
@@ -199,15 +241,140 @@ get_header('archive'); ?>
                                             } ?>
                                             
                                             
-                                        </div>
-                                    </article> <?php
+                            </div>
+                        </article> <?php
                                 }
                             }
                         ?>
 
                     </div>
+
+                    <div class="aircraft_sidebar_wrapper sidebar_wrapper" id="project_sidebar_wrapper">
+                        <aside id="secondary" class="widget-area">
+	                       <section id="search-2" class="widget widget_search">
+                                <h2 class="widget-title sidebar_title">Aircraft Specifications</h2>
+                                <div class="spec_item">
+                                    <strong class="spec_title">Year:</strong>
+                                    <p class="spec_value"><?php echo $year; ?></p>
+                                </div>
+                                <div class="spec_item">
+                                    <strong class="spec_title">Manufacturer:</strong>
+                                    <p class="spec_value"><?php echo $manufacturer; ?></p>
+                                </div>
+                                <div class="spec_item">
+                                    <strong class="spec_title">Model:</strong>
+                                    <p class="spec_value"><?php echo $model; ?></p>
+                                </div>
+                                <div class="spec_item">
+                                    <strong class="spec_title">Serial Number:</strong>
+                                    <p class="spec_value"><?php echo $serial_number; ?></p>
+                                </div>
+                                <div class="spec_item">
+                                    <strong class="spec_title">Registration Number:</strong>
+                                    <p class="spec_value"><?php echo $registration_number; ?></p>
+                                </div>
+                                <div class="spec_item">
+                                    <strong class="spec_title">Price:</strong>
+                                    <p class="spec_value"><?php echo $price; ?></p>
+                                </div>
+                                <div class="spec_item">
+                                    <strong class="spec_title">Location:</strong>
+                                    <p class="spec_value"><?php echo $location; ?></p>
+                                </div>
+                            </section>
+                            <section id="project_aircraft_details" class="widget">
+                                <h2 class="widget-title sidebar_title">Aircraft Details</h2>
+                                <?php if( $history ) { ?>
+                                <div class="detail_item">
+                                    <strong class="detail_title">History:</strong>
+                                    <p class="detail_value"><?php echo $history; ?></p>
+                                </div>
+                                <?php } ?>
+                                <?php if( $airframe ) { ?>
+                                <div class="detail_item">
+                                    <strong class="detail_title">Airframe:</strong>
+                                    <p class="detail_value"><?php echo $airframe; ?></p>
+                                </div>
+                                <?php } ?>
+                                <?php if( $engine ) { ?>
+                                <div class="detail_item">
+                                    <strong class="detail_title">Engine(s):</strong>
+                                    <p class="detail_value"><?php echo $engine; ?></p>
+                                </div>
+                                <?php } ?>
+                                <?php if( $propeller ) { ?>
+                                <div class="detail_item">
+                                    <strong class="detail_title">Propeller(s):</strong>
+                                    <p class="detail_value"><?php echo $propeller; ?></p>
+                                </div>
+                                <?php } ?>
+                                <?php if( $exterior ) { ?>
+                                <div class="detail_item">
+                                    <strong class="detail_title">Exterior:</strong>
+                                    <p class="detail_value"><?php echo $exterior; ?></p>
+                                </div>
+                                <?php } ?>
+                                <?php if( $interior ) { ?>
+                                <div class="detail_item">
+                                    <strong class="detail_title">Interior:</strong>
+                                    <p class="detail_value"><?php echo $interior; ?></p>
+                                </div>
+                                <?php } ?>
+                                <?php if( $avionics ) { ?>
+                                <div class="detail_item">
+                                    <strong class="detail_title">Avionics:</strong>
+                                    <p class="detail_value"><?php echo $avionics; ?></p>
+                                </div>
+                                <?php } ?>
+                                <?php if( $equipment ) { ?>
+                                <div class="detail_item">
+                                    <strong class="detail_title">Equipment:</strong>
+                                    <p class="detail_value"><?php echo $equipment; ?></p>
+                                </div>
+                                <?php } ?>
+                                <?php if( $misc ) { ?>
+                                <div class="detail_item">
+                                    <strong class="detail_title">Miscellaneous:</strong>
+                                    <p class="detail_value"><?php echo $misc; ?></p>
+                                </div>
+                                <?php } ?>
+                            </section>
+                        </aside><!-- #secondary -->
+                    </div> <!-- #project_sidebar_wrapper -->
                 </div>
-            </div>
+                <div class="aircraft_gallery" id="gallery">
+                    <div class="page_main_wrapper" id="project_gallery_main_wrapper">
+                <?php if($project_gallery) { ?>
+                        <div class="flexslider gallery_slider" id="gallery_slider">
+                                <ul class="slides gallery_slides">
+                                    <?php foreach( $project_gallery as $row ) {
+                                                    $image = $row['project_gallery_image'];
+                                                    $title = $image['title'];
+                                                    $description = $image['description'];
+                                                    $caption = $image['caption'];
+                                                    $url = $image['url'];
+                                                    $alt = $image['alt'];                           
+                                    ?>
+                                    <li data-thumb="<?php echo $url; ?>">
+                                        <div class="aircraft_slider_image">
+                                             <img src="<?php echo $url; ?>" alt="<?php echo $alt; ?>"/>
+                                        </div>
+                                    </li>
+                                    
+                                    <?php  } ?>
+                                </ul>
+                            <span id='scroll_L_Arrow' class="scroll_arrow" onclick="scrollThumb('Go_L', 'gallery')"><i class="fa fa-chevron-circle-left" aria-hidden="true"></i></span>
+                            <span id='scroll_R_Arrow' class="scroll_arrow" onclick="scrollThumb('Go_R', 'gallery')"><i class="fa fa-chevron-circle-right" aria-hidden="true"></i></span>
+                            <span id='scroll_U_Arrow' class="scroll_arrow" onclick="scrollThumb('Go_U', 'gallery')"><i class="fa fa-chevron-circle-up" aria-hidden="true"></i></span>
+                            <span id='scroll_D_Arrow' class="scroll_arrow" onclick="scrollThumb('Go_D', 'gallery')"><i class="fa fa-chevron-circle-down" aria-hidden="true"></i></span>
+                           </div> <?php
+                        } 
+                ?>
+                    </div><!-- #project_gallery_main_wrapper -->
+                </div><!-- #gallery -->
+            </div><!-- #project_section-->
+<!--
+
             <div class="aircraft_gallery" id="gallery">
                 <div class="aircraft_section_heading">
                     <div class="aircraft_content_wrap">
@@ -287,6 +454,7 @@ get_header('archive'); ?>
                 <div class="aircraft_section_heading">
                     <div class="aircraft_content_wrap">
                         <h2>details</h2>
+                        
                     </div>
                 </div>
                 <span class="aircraft_section_line"></span>
@@ -422,6 +590,7 @@ get_header('archive'); ?>
                     wp_reset_postdata();
                 }
         ?>
+-->
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
---
+++ template-parts/content-project.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Hangar_49
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <div class="article_side">
+        <p><?php echo get_the_date(); ?></p>
+    </div>
+    <div class="article_preview_content">
+        <header class="article_header" id="single_news_post_header">
+                <?php
+                if ( is_singular() ) : ?>
+                <div class="article_title">
+                <?php    
+                    the_title( '<h1 class="entry-title">', '</h1>' );
+                else : ?>
+                <div class="article_excerpt_title">
+                <?php    
+                    the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+                endif;     
+                ?>
+            </div><!-- .article_title .article_excerpt_title -->
+            <?php 
+                if ( 'post' === get_post_type() ) : ?>
+            <div class="entry-meta">
+                <ul class="article_meta_list">
+                    <li><i class="far fa-user"></i> by <?php echo get_the_author('display_name'); ?></li>
+                    <li><i class="far fa-calendar"></i> on  <?php echo get_the_date(); ?></li>
+                </ul>
+                <div class="article_category_list">
+                    <i class="fas fa-tags"></i> <?php echo get_the_category_list(); ?>
+                </div>
+            </div><!-- .entry-meta -->
+            <?php endif; ?>       
+        </header>    
+        <?php if ( has_post_thumbnail() ) { ?>
+        <div class="article_media">
+            <?php 
+                $thumbnail_id = get_post_thumbnail_id( $post->ID );
+                $title = get_post(get_post_thumbnail_id())->post_title; 
+                $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true); 
+                $caption = get_post(get_post_thumbnail_id())->post_excerpt;   
+                $description = get_post(get_post_thumbnail_id())->post_content; // The Description
+            ?>
+            <figure>
+                <img src="<?php the_post_thumbnail_url( 'medium_large' );  ?>" title="<?php echo $title ?>" alt="<?php echo $alt; ?>"/>
+                <?php if ($caption !== "") { ?>
+                <figcaption><?php echo $caption; ?></figcaption>
+                <?php } ?>
+            </figure>          
+        </div><!-- .article_media -->
+        <?php } else {} ?>
+        <div class="news_article_content">
+        <?php if ( is_singular() ) : ?>
+            <?php
+                the_content( sprintf(
+                    wp_kses(
+                        /* translators: %s: Name of current post. Only visible to screen readers */
+                        __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'hangar49' ),
+                        array(
+                            'span' => array(
+                                'class' => array(),
+                            ),
+                        )
+                    ),
+                    get_the_title()
+                ) );
+
+                wp_link_pages( array(
+                    'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'hangar49' ),
+                    'after'  => '</div>',
+                ) );
+            
+        else : ?>
+            <div class="article_excerpt">
+                 <?php the_excerpt(); ?>
+            </div> <?php
+        endif; ?> 
+<!--        </div> .news_article_content    -->
+        <footer class="entry-footer">
+
+<!--            <?php hangar49_entry_footer(); ?>-->
+        </footer><!-- .entry-footer -->    
+    </div><!-- .article_preview_content -->
+</article><!-- #post-<?php the_ID(); ?> -->
